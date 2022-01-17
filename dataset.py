0ed55ec1619b7a519fa449e696e2346aab04bc00
Damian Stetson <damianstetson17@gmail.com>
2022-01-17 00:01:32 -0300

    first commit

---
+++ dataset.py
@@ -0,0 +1,20 @@
+diana_list = [
+        {"name":"Dr. Sueno", "author":"Stephen  King"},
+        {"name":"El Visitante", "author":"Stephen  King"},
+        {"name":"El instituto", "author":"Stephen  King"},
+        {"name":"Apocalipsis", "author":"Stephen  King"}
+    ]
+
+damian_list = [
+        {"name":"Robinson Crusoe", "author":"Daniel Defoe"},
+        {"name":"Mi planta de naranja lima", "author":"Jose Mauro de Vasconcelos"},
+    ]
+
+angy_list = [
+        {"name":"Cumbres borrascosas", "author":"Emily Bronte"},
+        {"name":"Los ojos del perro siberiano", "author":"Antonio Santana"},
+        {"name":"El almohadon de plumas", "author":"Horacio Quiroga"},
+        {"name":"Cuentos de Locura, de amor y de muerte", "author":"Horacio Quiroga"},
+    ]
+    
+libros = diana_list + damian_list + angy_list
---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, json, jsonify
+from werkzeug.wrappers import response
+from dataset import *
+
+app = Flask(__name__)
+
+@app.route('/books/favorites/angy')
+def libros_angy():
+    return jsonify(angy_list)
+
+@app.route('/books/favorites/diana')
+def libros_diana():
+    return jsonify(diana_list)
+
+@app.route('/books/favorites/damian')
+def libros_damian():
+    return jsonify(damian_list)
+
+@app.route('/books/find/all')
+def get_all_libro():
+        if len(libros) > 0:
+            return jsonify(libros) 
+        else:
+            return jsonify({'result':'null'})
+
+@app.route('/books/find/<string:book_name>')
+def get_libro(book_name):
+
+    if len(libros) > 0:
+        response = jsonify({'result':'null'}) 
+        for book in libros:
+            if book['name'] == book_name:
+                response = jsonify(book)
+            return response
+    else:
+        return jsonify({'result':'null'}) 
+
+if __name__ == '__main__':
+    app.run(debug = True, port=4000)
